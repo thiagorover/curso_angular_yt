f5bab26b9060e73ece5f95660f291641a46b4f67
Thiago Henrique Rover <43826096+thiagorover@users.noreply.github.com>
2023-03-14 23:53:58 -0300

    Branch da terceira aula de angular

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent {
+  name: string = 'Rover';
+  age: number = 25;
+  job = 'Programador'
+  hobbies = ['Futebol', 'Academia', 'Estudar'];
+  car = {
+    name: 'Renault',
+    year: 2014
+  }
+
+  constructor(){}
+
+  ngOnInit(): void{}
 
 }
